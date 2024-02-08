235c84a66e95b681eb75b925f2191f11bc42de0b
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-02-08 09:24:06 +0200

    Mongoose contacts schema

---
+++ backend/src/models/contactModels.js
@@ -0,0 +1,29 @@
+// src/models/contactModels.js
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = Contact;
