41bcfe26f5cc7faf43fe39083c65adfc9bbf1e2b
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-02-10 23:11:08 +0200

    Include CORS and middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,29 @@
-// Import the Express module
+// src/server.js
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const contactRoutes = require('./routes/contactRoutes');
 
-// Create an Express application
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Define a route handler for the root path
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-// Start the server, listening on port 3000
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+// Routes
+app.use('/api/contacts', contactRoutes);
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/MehdiDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch(error => console.error('Error connecting to MongoDB:', error));
+
