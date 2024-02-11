808daee4a829c7d41db94df50d93e7977178d782
El Mahdi Amerian <mehdiamerian@gmail.com>
2024-02-12 00:09:44 +0200

    Setting up server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,39 @@
-// src/server.js
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const contactRoutes = require('./routes/contactRoutes');
+const mongoose = require('mongoose');
+const contactRoutes = require('./routes/contactRoutes.js'); 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware
+const mongoURL = 'mongodb://localhost:27017/contact';
+
+// Define CORS options
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allowed origin
+  optionsSuccessStatus: 200, // Success status for preflight requests
+};
+
+// Route handler function
+app.get('/contact/.', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is a CORS-enabled function' });
+});
+
+// Applying CORS 
 app.use(cors());
-app.use(express.json());
 
-// Routes
-app.use('/api/contacts', contactRoutes);
+// Importing contactRoutes
+app.use('/', contactRoutes);
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/MehdiDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+mongoose.connect(mongoURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your Express server once connected to MongoDB
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
   });
-})
-.catch(error => console.error('Error connecting to MongoDB:', error));
-
